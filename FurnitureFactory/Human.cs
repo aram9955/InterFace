20d37198fbfb68afdc447b23267e0ce1ebc2c008
Aram Baghdasarian <aram99551@gmail.com>
2021-09-29 16:51:45 +0400

    Add project files.

---
+++ FurnitureFactory/Human.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FurnitureFactory
+{
+    public interface IWalk
+    {
+        void Walk();
+    }
+    public interface IReport
+    {
+        void Report();
+    }
+    public class FilmReporter :IReport
+    {
+        public void Report()
+        {
+            throw new NotImplementedException();
+        }
+}
+    public class Reporter : IWalk, IReport
+    {
+        public void Report()
+        {
+            throw new NotImplementedException();
+        }
+        public void Walk()
+        {
+            throw new NotImplementedException();
+
+        }
+        public class Doctor : IWalk
+        {
+            public void Walk()
+            {
+                throw new NotImplementedException();
+
+            }
+        }
+        public class Casier : IWalk
+        {
+            public void Walk()
+            {
+                throw new NotImplementedException();
+
+            }
+        }
+    }
+}
---
+++ FurnitureFactory/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using static FurnitureFactory.Reporter;
+
+namespace FurnitureFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IReport reporter1 = new Reporter();
+            IReport reporter2 = new FilmReporter();
+
+            List<IReport> reporters = new List<IReport>();
+
+            reporters.Add(reporter1);
+            reporters.Add(reporter2);
+            reporters[0].Report();
+
+            List<IWalk> walkers = new List<IWalk>();
+
+            IWalk walker1 = new Doctor();
+
+            walkers.Add(walker1);
+            walkers.Add((IWalk)reporter1);
+            walkers[0].Walk();
+
+
+
+        }
+    }
+}
